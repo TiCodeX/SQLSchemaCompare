6e885547d9dd5b0b2cfabcbcfa44030e52d9a786
Davide Corda <davide1981@gmail.com>
2018-04-19 16:09:48 +0200

    Add NLog logger provider

--- SQLCompare.UI/Pages/Index.cshtml.cs
+++ SQLCompare.UI/Pages/Index.cshtml.cs
@@ -1,12 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
 
 namespace SQLCompare.UI.Pages
 {
     public class IndexModel : PageModel
     {
+        private ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
         public void OnGet()
         {
             ViewData["Title"] = "Pluto";
+            _logger.LogError("This is an error!");
         }
     }
 }
--- SQLCompare.UI/Program.cs
+++ SQLCompare.UI/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Server.Kestrel.Https;
 using Microsoft.Extensions.Logging;
+using NLog.Web;
 using System;
 using System.IO;
 using System.Net;
@@ -16,46 +17,59 @@ namespace SQLCompare.UI
 
         public static void Main()
         {
+            // NLog: setup the logger first to catch all errors
+            var logger = NLog.Web.NLogBuilder.ConfigureNLog("NLog.config").GetCurrentClassLogger();
             try
             {
-                //Logger.LogMessage("Starting WebServer...");
-
-                var host = new WebHostBuilder()
-                    .UseKestrel(options =>
-                    {
-                        options.Listen(IPAddress.Any, WebServerPort, listenOptions =>
-                        {
-
-                            using (var resourceStream = Assembly.GetExecutingAssembly().GetManifestResourceStream("SQLCompare.UI.certificate.pfx"))
-                            using (var memoryStream = new MemoryStream())
-                            {
-                                resourceStream.CopyTo(memoryStream);
-
-                                listenOptions.UseHttps(new HttpsConnectionAdapterOptions()
-                                {
-                                    ServerCertificate = new X509Certificate2(memoryStream.ToArray(), "test1234"),
-                                    SslProtocols = SslProtocols.Tls12
-                                    //other settings???
-                                });
-                            }
-                        });
-                    })
-                    .UseContentRoot(Directory.GetCurrentDirectory())
-                    .ConfigureLogging((hostingContext, logging) =>
-                    {
-                        logging.AddConsole();
-                        logging.AddDebug();
-                    })
-                    .UseStartup<WebServerStartup>()
-                    .Build();
-
-                host.Run();
+                logger.Debug("init main");
+                BuildWebHost().Run();
             }
-            catch (Exception)
+            catch (Exception ex)
             {
-                //Logger.LogError("An error occurred while initializing the Webserver : " + ex.Message);
+                //NLog: catch setup errors
+                logger.Error(ex, "Stopped program because of exception");
+                throw;
             }
+            finally
+            {
+                // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
+                NLog.LogManager.Shutdown();
+            }
+
         }
 
+        private static IWebHost BuildWebHost()
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel(options =>
+                {
+                    options.Listen(IPAddress.Any, WebServerPort, listenOptions =>
+                    {
+
+                        using (var resourceStream = Assembly.GetExecutingAssembly().GetManifestResourceStream("SQLCompare.UI.certificate.pfx"))
+                        using (var memoryStream = new MemoryStream())
+                        {
+                            resourceStream.CopyTo(memoryStream);
+
+                            listenOptions.UseHttps(new HttpsConnectionAdapterOptions()
+                            {
+                                ServerCertificate = new X509Certificate2(memoryStream.ToArray(), "test1234"),
+                                SslProtocols = SslProtocols.Tls12
+                                //other settings???
+                            });
+                        }
+                    });
+                })
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseStartup<WebServerStartup>()
+                .ConfigureLogging(logging =>
+                {
+                    logging.ClearProviders();
+                    logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
+                })
+                .UseNLog()
+                .Build();
+            return host;
+        }
     }
 }

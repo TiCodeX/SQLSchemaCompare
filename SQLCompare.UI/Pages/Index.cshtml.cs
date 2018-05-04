0344609371f43170d50238a6b91ad09efd57fcbb
Daniele Debernardi <drebrez@gmail.com>
2018-05-04 16:58:06 +0200

    Add comments to the Index page

--- SQLCompare.UI/Pages/Index.cshtml.cs
+++ SQLCompare.UI/Pages/Index.cshtml.cs
@@ -3,19 +3,28 @@ using Microsoft.Extensions.Logging;
 
 namespace SQLCompare.UI.Pages
 {
+    /// <summary>
+    /// PageModel of the Index page
+    /// </summary>
     public class Index : PageModel
     {
-        private readonly ILogger<Index> _logger;
+        private readonly ILogger<Index> logger;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Index"/> class.
+        /// </summary>
+        /// <param name="logger">The injected Logger</param>
         public Index(ILogger<Index> logger)
         {
-            _logger = logger;
+            this.logger = logger;
         }
 
+        /// <summary>
+        /// Initialize the state of the page
+        /// </summary>
         public void OnGet()
         {
-            ViewData["Title"] = "Pluto";
-            _logger.LogError("This is an error!");
+            this.ViewData["Title"] = "Pluto";
         }
     }
 }

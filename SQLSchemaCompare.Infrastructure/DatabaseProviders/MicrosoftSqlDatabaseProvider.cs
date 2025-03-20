613b722efd19dc2bd13434435092495e40646eed
Daniele Debernardi <drebrez@gmail.com>
2025-03-20 12:59:49 +0100

    Support included columns in MicrosoftSQL indexes

--- SQLSchemaCompare.Infrastructure/DatabaseProviders/MicrosoftSqlDatabaseProvider.cs
+++ SQLSchemaCompare.Infrastructure/DatabaseProviders/MicrosoftSqlDatabaseProvider.cs
@@ -207,6 +207,7 @@
             query.AppendLine("       CAST(ic.key_ordinal AS bigint) AS 'OrdinalPosition',");
             query.AppendLine("       i.type AS Type,");
             query.AppendLine("       i.is_unique AS 'IsUnique',");
+            query.AppendLine("       ic.is_included_column AS 'IsIncluded',");
             query.AppendLine("       i.filter_definition AS 'FilterDefinition'");
             query.AppendLine("FROM sys.indexes i");
             query.AppendLine("JOIN sys.index_columns ic ON i.object_id = ic.object_id AND i.index_id = ic.index_id");

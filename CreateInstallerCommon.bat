7594152dd00de206836a3bf29e85628bf838136f
Daniele Debernardi <drebrez@gmail.com>
2026-04-23 23:39:53 +0200

    draft5

--- CreateInstallerCommon.bat
+++ CreateInstallerCommon.bat
@@ -6,6 +6,9 @@ if not exist "%VsDevCmdPath%" (
   set "VsDevCmdPath=C:\Program Files\Microsoft Visual Studio\2022\Enterprise\Common7\Tools\VsDevCmd.bat"
 )
 if not exist "%VsDevCmdPath%" (
+  set "VsDevCmdPath=C:\Program Files\Microsoft Visual Studio\18\Enterprise\Common7\Tools\VsDevCmd.bat"
+)
+if not exist "%VsDevCmdPath%" (
   set "VsDevCmdPath=C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\Common7\Tools\VsDevCmd.bat"
 )
 if not exist "%VsDevCmdPath%" (
